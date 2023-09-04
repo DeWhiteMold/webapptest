8a723f4df996b90434412c97058d5c369e50bae5
Nikolai Smirnov <dewhitemold@yandex.ru>
2023-09-04 14:57:45 +0200

    brah

---
+++ src/Api.js
@@ -0,0 +1,72 @@
+import { baseUrl } from "./consts";
+
+class Api {
+  constructor(url) {
+    this._url = url;
+  }
+
+  _getResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  getText(lang, user) {
+    return fetch(`${this._url}/text`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        bot_lang: lang,
+        ...(user && {user: user})
+      })
+    })
+    .then(res => this._getResponse(res))
+  }
+
+  checkSub(lang, userId) {
+    return fetch(`${this._url}/check_subscribe`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        bot_lang: lang,
+        user_id: userId,
+      })
+    })
+    .then(res => this._getResponse(res))
+  }
+
+  newSub(lang, userId) {
+    return fetch(`${this._url}/advert_url`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        bot_lang: lang,
+        user_id: userId,
+      })
+    })
+    .then(res => this._getResponse(res))
+  }
+
+  withdraw(lang, userId) {
+    return fetch(`${this._url}/withdraw`, {
+      method: 'POST',
+      body: JSON.stringify({
+        bot_lang: lang,
+        user_id: userId,
+      })
+    })
+    .then(res => this._getResponse(res))
+  }
+}
+
+const api = new Api(baseUrl)
+
+export default api
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useCallback, useEffect, useState } from 'react';
 import './App.scss';
 import MySvgComponent from './cardIcon';
-import { cardRegExp, numberRegExp } from './consts';
+import { cardRegExp } from './consts';
+import api from './Api';
 
 const tg = window.Telegram.WebApp
+const lang = 'it'
 
 function App() {
   const [isCardSelected, setIsCardSelected] = useState(false)
+  const [isStatOpen, setIsStatOpen] = useState(false)
+  const [texts, setTexts] = useState(null)
+  const [isSub, setIsSub] = useState(false)
+  const [user, setUser] = useState(null)
   const [cardNumber,setCardNumber] = useState('')
   const [withdrawValue, setWithdrawValue] = useState('')
 
@@ -18,20 +24,39 @@ function App() {
     setWithdrawValue(e.target.value)
   }
 
+  const handleAdvSub = () => {
+    api.newSub(lang, user.id)
+      .then(res => {
+        if(res) {
+          console.log(res)
+          setIsSub(true)
+        }
+      })
+      // .then(() => {
+      //   api.checkSub(lang, user.id)
+      //   .then(res => console.log(res))
+      // }) 
+  }
+
   useEffect(() => {
-    if(cardRegExp.test(cardNumber) && withdrawValue > 0) {
+    if(cardRegExp.test(cardNumber) && withdrawValue > 0 && isSub) {
       tg.MainButton.show()
     } else {
       tg.MainButton.hide()
     }
-  }, [cardNumber, withdrawValue])
+  }, [cardNumber, withdrawValue, isSub])
 
   const onSendData = useCallback(() => {
-    console.log(withdrawValue, cardNumber)
-  }, [withdrawValue, cardNumber])
+    console.log(withdrawValue, cardNumber, user)
+  }, [withdrawValue, cardNumber, user])
 
   useEffect(() => {
     tg.ready()
+    setUser(tg.initDataUnsafe.user)
+    api.getText(lang)
+      .then(res => {
+        setTexts(res)
+      })
   }, [])
 
   useEffect(() => {
@@ -39,36 +64,58 @@ function App() {
     return () => {tg.offEvent('mainButtonClicked', onSendData)}
   })
 
-  return (
+  return texts !== null && (
     <div className="app">
       <header className="header">
-        <span className="header__caption">Withdraw</span>
-        <button className="header__exit-btn" onClick={tg.close}>close</button>
+        <span className="header__caption">{texts.select_payment}</span>
+        <button className="header__exit-btn" onClick={tg.close}>{texts.withdraw_cancel}</button>
       </header>
       {
         isCardSelected ?
         <form className="card-form">
-          <input type="number" className='card-form__input' placeholder='card number' value={cardNumber} onChange={handleCardInput} maxLength='16'/>
-          <input type="number" className='card-form__input' placeholder='amount' value={withdrawValue} onChange={handleWithdrawInput}/>
-          <span className="card-form__adv">reklamniy kal</span>
-          <button className="card-form__adv-btn">knopka skam</button>
+          <input type="number" className='card-form__input' placeholder={texts.card_data_placeholder} value={cardNumber} onChange={handleCardInput} maxLength='16'/>
+          <input type="number" className='card-form__input' placeholder={texts.amount_placeholder} value={withdrawValue} onChange={handleWithdrawInput}/>
+          <span className="card-form__adv">{texts.advertisement_button_text}</span>
+          <button type='button' className="card-form__adv-btn" onClick={handleAdvSub}>{texts.advertising_button}</button>
         </form> :
-        <div className="cards">
-          <div className="card">
-            <MySvgComponent />
-            <button className="card__select-btn" onClick={() => {setIsCardSelected(true)}}>select</button>
+        <div className="main">
+          <div className="cards">
+            <div className="card">
+              <MySvgComponent />
+              <button className="card__select-btn" onClick={() => {setIsCardSelected(true)}}>{texts.withdrawal_method_1}</button>
+            </div>
+            <div className="card">
+              <MySvgComponent />
+              <button className="card__select-btn" onClick={() => {setIsCardSelected(true)}}>{texts.withdrawal_method_2}</button>
+            </div>
+            {
+              texts.withdrawal_method_3 && 
+              <div className="card">
+                <MySvgComponent />
+                <button className="card__select-btn" onClick={() => {setIsCardSelected(true)}}>{texts.withdrawal_method_2}</button>
+              </div>
+            }
+            {
+              texts.withdrawal_method_4 && 
+              <div className="card">
+                <MySvgComponent />
+                <button className="card__select-btn" onClick={() => {setIsCardSelected(true)}}>{texts.withdrawal_method_4}</button>
+              </div>
+            }
+            {
+              texts.withdrawal_method_5 && 
+              <div className="card">
+                <MySvgComponent />
+                <button className="card__select-btn" onClick={() => {setIsCardSelected(true)}}>{texts.withdrawal_method_5}</button>
+              </div>
+            }
           </div>
-          <div className="card">
-            <MySvgComponent />
-            <button className="card__select-btn" onClick={() => {setIsCardSelected(true)}}>select</button>
-          </div>
-          <div className="card">
-            <MySvgComponent />
-            <button className="card__select-btn" onClick={() => {setIsCardSelected(true)}}>select</button>
-          </div>
-          <div className="card">
-            <MySvgComponent />
-            <button className="card__select-btn" onClick={() => {setIsCardSelected(true)}}>select</button>
+          <div className="stats">
+            {
+              isStatOpen ? 
+                <span className="stats__info">{texts.statistic_to_user}</span> :
+                <button className="stats__btn" onClick={() => {setIsStatOpen(true)}}>{texts.main_statistic}</button>
+            }
           </div>
         </div>
       }
